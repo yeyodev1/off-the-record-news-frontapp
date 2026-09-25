7a3273cbed9776707d0f8ec188150cd38fee8454
Diego Reyes <diegorele13@gmail.com>
2026-09-24 23:08:57 -0500

    chore: toma los textos de interfaz desde site.ts

--- src/components/news/TipForm.vue
+++ src/components/news/TipForm.vue
@@ -3,7 +3,7 @@ import { site } from '@/config/site'
 import { useTipForm, TIP_MIN_LENGTH } from '@/composables/useTipForm'
 import FormField from './FormField.vue'
 import FormSuccess from './FormSuccess.vue'
-import { ui } from './uiCopy'
+import { ui } from '@/config/site'
 
 const { form, length, isLongEnough, submitting, error, doneMessage, submit } = useTipForm()
 </script>
